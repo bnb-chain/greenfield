99e43e21a966a8e484cd524eab08d8d5d130a677
yutianwu <wzxingbupt@gmail.com>
2023-01-05 14:17:18 +0800

    fix command line

--- x/bridge/client/cli/tx_transfer_out.go
+++ x/bridge/client/cli/tx_transfer_out.go
@@ -17,7 +17,7 @@ func CmdTransferOut() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "transfer-out [from]",
 		Short: "Broadcast message transfer-out",
-		Args:  cobra.ExactArgs(3),
+		Args:  cobra.ExactArgs(2),
 		RunE: func(cmd *cobra.Command, args []string) (err error) {
 			clientCtx, err := client.GetClientTxContext(cmd)
 			if err != nil {
@@ -33,7 +33,7 @@ func CmdTransferOut() *cobra.Command {
 			if err != nil {
 				return err
 			}
-			
+
 			msg := types.NewMsgTransferOut(
 				clientCtx.GetFromAddress().String(),
 				toAddr.String(),
