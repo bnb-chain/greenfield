6b6c7defe594db9cfb8a72cc677885bbca944cf1
yutianwu <wzxingbupt@gmail.com>
2023-01-09 20:05:53 +0800

    fix unit test errors

--- x/bridge/types/genesis_test.go
+++ x/bridge/types/genesis_test.go
@@ -19,12 +19,24 @@ func TestGenesisState_Validate(t *testing.T) {
 			valid:    true,
 		},
 		{
-			desc:     "valid genesis state",
+			desc: "valid genesis state",
 			genState: &types.GenesisState{
-
+				Params: types.Params{
+					TransferOutRelayerFee: "1",
+				},
 				// this line is used by starport scaffolding # types/genesis/validField
 			},
 			valid: true,
+		},
+		{
+			desc: "invalid genesis state",
+			genState: &types.GenesisState{
+				Params: types.Params{
+					TransferOutRelayerFee: "-1",
+				},
+				// this line is used by starport scaffolding # types/genesis/validField
+			},
+			valid: false,
 		},
 		// this line is used by starport scaffolding # types/genesis/testcase
 	} {
