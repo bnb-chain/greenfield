5e82505fefe84c40d82c2064fab8278c067bfdf8
j75689 <j75689@gmail.com>
2023-02-17 11:32:45 +0800

    refactor: rewrite the NewGreenfieldClient function by option pattern

--- sdk/client/gnfd_client.go
+++ sdk/client/gnfd_client.go
@@ -25,7 +25,6 @@ import (
 	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
 	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
 )
 
 type AuthQueryClient = authtypes.QueryClient
@@ -46,7 +45,6 @@ type SlashingQueryClient = slashingtypes.QueryClient
 type StakingQueryClient = stakingtypes.QueryClient
 type TxClient = tx.ServiceClient
 type UpgradeQueryClient = upgradetypes.QueryClient
-
 type GreenfieldClient struct {
 	AuthQueryClient
 	AuthzQueryClient
@@ -69,12 +67,15 @@ type GreenfieldClient struct {
 	keyManager keys.KeyManager
 	chainId    string
 	codec      *codec.ProtoCodec
+
+	// option field
+	grpcDialOption []grpc.DialOption
 }
 
-func grpcConn(addr string) *grpc.ClientConn {
+func grpcConn(addr string, opts ...grpc.DialOption) *grpc.ClientConn {
 	conn, err := grpc.Dial(
 		addr,
-		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		opts...,
 	)
 	if err != nil {
 		panic(err)
@@ -82,38 +83,35 @@ func grpcConn(addr string) *grpc.ClientConn {
 	return conn
 }
 
-func NewGreenfieldClient(grpcAddr, chainId string) GreenfieldClient {
-	conn := grpcConn(grpcAddr)
-	cdc := types.Cdc()
-	return GreenfieldClient{
-		authtypes.NewQueryClient(conn),
-		authztypes.NewQueryClient(conn),
-		banktypes.NewQueryClient(conn),
-		crosschaintypes.NewQueryClient(conn),
-		distrtypes.NewQueryClient(conn),
-		feegranttypes.NewQueryClient(conn),
-		gashubtypes.NewQueryClient(conn),
-		paymenttypes.NewQueryClient(conn),
-		sptypes.NewQueryClient(conn),
-		bridgetypes.NewQueryClient(conn),
-		storagetypes.NewQueryClient(conn),
-		govv1.NewQueryClient(conn),
-		oracletypes.NewQueryClient(conn),
-		paramstypes.NewQueryClient(conn),
-		slashingtypes.NewQueryClient(conn),
-		stakingtypes.NewQueryClient(conn),
-		tx.NewServiceClient(conn),
-		upgradetypes.NewQueryClient(conn),
-		nil,
-		chainId,
-		cdc,
+func NewGreenfieldClient(grpcAddr, chainId string, opts ...GreenfieldClientOption) *GreenfieldClient {
+	client := &GreenfieldClient{
+		chainId: chainId,
+		codec:   types.Cdc(),
 	}
-}
+	for _, opt := range opts {
+		opt.Apply(client)
+	}
 
-func NewGreenfieldClientWithKeyManager(grpcAddr, chainId string, keyManager keys.KeyManager) GreenfieldClient {
-	gnfdClient := NewGreenfieldClient(grpcAddr, chainId)
-	gnfdClient.keyManager = keyManager
-	return gnfdClient
+	conn := grpcConn(grpcAddr, client.grpcDialOption...)
+	client.AuthQueryClient = authtypes.NewQueryClient(conn)
+	client.AuthzQueryClient = authztypes.NewQueryClient(conn)
+	client.BankQueryClient = banktypes.NewQueryClient(conn)
+	client.CrosschainQueryClient = crosschaintypes.NewQueryClient(conn)
+	client.DistrQueryClient = distrtypes.NewQueryClient(conn)
+	client.FeegrantQueryClient = feegranttypes.NewQueryClient(conn)
+	client.GashubQueryClient = gashubtypes.NewQueryClient(conn)
+	client.PaymentQueryClient = paymenttypes.NewQueryClient(conn)
+	client.SpQueryClient = sptypes.NewQueryClient(conn)
+	client.BridgeQueryClient = bridgetypes.NewQueryClient(conn)
+	client.StorageQueryClient = storagetypes.NewQueryClient(conn)
+	client.GovQueryClientV1 = govv1.NewQueryClient(conn)
+	client.OracleQueryClient = oracletypes.NewQueryClient(conn)
+	client.ParamsQueryClient = paramstypes.NewQueryClient(conn)
+	client.SlashingQueryClient = slashingtypes.NewQueryClient(conn)
+	client.StakingQueryClient = stakingtypes.NewQueryClient(conn)
+	client.UpgradeQueryClient = upgradetypes.NewQueryClient(conn)
+	client.TxClient = tx.NewServiceClient(conn)
+	return client
 }
 
 func (c *GreenfieldClient) GetKeyManager() (keys.KeyManager, error) {
@@ -121,10 +119,6 @@ func (c *GreenfieldClient) GetKeyManager() (keys.KeyManager, error) {
 		return nil, types.KeyManagerNotInitError
 	}
 	return c.keyManager, nil
-}
-
-func (c *GreenfieldClient) SetKeyManager(keyManager keys.KeyManager) {
-	c.keyManager = keyManager
 }
 
 func (c *GreenfieldClient) SetChainId(id string) {
