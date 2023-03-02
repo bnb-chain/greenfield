8dc8a8bb1e05d1fc9b45fb48283cf1ddf19045bf
Alexgao001 <alex.g@nodereal.io>
2023-03-02 20:09:21 +0800

    fix comments

--- x/storage/types/types.go
+++ x/storage/types/types.go
@@ -22,30 +22,28 @@ func DecodeSequence(bz []byte) Uint {
 }
 
 func (m *BucketInfo) ToNFTMetadata() (*MetaData, error) {
-	return toNFTMetaData(*m, []string{"PaymentOutFlows"})
+	return toNFTMetaData(*m)
 }
 
 func (m *ObjectInfo) ToNFTMetadata() (*MetaData, error) {
-	return toNFTMetaData(*m, []string{"Checksums"})
+	return toNFTMetaData(*m)
 }
 
 func (m *GroupInfo) ToNFTMetadata() (*MetaData, error) {
-	return toNFTMetaData(*m, nil)
+	return toNFTMetaData(*m)
 }
 
-func toNFTMetaData(m interface{}, omitFields []string) (*MetaData, error) {
+func toNFTMetaData(m interface{}) (*MetaData, error) {
 	attributes := make([]Trait, 0)
 	v := reflect.ValueOf(m)
 	typ := v.Type()
 
-OUTER:
 	for i := 0; i < v.NumField(); i++ {
 		field := v.Field(i)
 		fieldName := typ.Field(i).Name
-		for _, n := range omitFields {
-			if fieldName == n {
-				continue OUTER
-			}
+		tag := typ.Field(i).Tag
+		if tag.Get("traits") == "omit" {
+			continue
 		}
 		value := fmt.Sprintf("%v", field.Interface())
 		attributes = append(attributes,
