2dca10fef67d19b59798856f28030b44d537a1e4
Fynn <zcheng1004@gmail.com>
2023-03-22 09:51:59 +0800

    remove some todo

--- x/storage/types/query.pb.gw.go
+++ x/storage/types/query.pb.gw.go
@@ -1904,7 +1904,7 @@ var (
 
 	pattern_Query_HeadGroup_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 3, 1, 0, 4, 1, 5, 4}, []string{"greenfield", "storage", "head_group", "group_owner", "group_name"}, "", runtime.AssumeColonVerbOpt(false)))
 
-	pattern_Query_ListGroup_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 1, 0, 4, 1, 5, 4}, []string{"bnb-chain", "greenfield", "storage", "list_group", "group_owner"}, "", runtime.AssumeColonVerbOpt(false)))
+	pattern_Query_ListGroup_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 3}, []string{"greenfield", "storage", "list_group", "group_owner"}, "", runtime.AssumeColonVerbOpt(false)))
 
 	pattern_Query_HeadGroupMember_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"bnb-chain", "greenfield", "storage", "head_group_member"}, "", runtime.AssumeColonVerbOpt(false)))
 
