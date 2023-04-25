d334b059e169a9e73e60017a83a7bc469df60218
Chris Li <271678682li@gmail.com>
2023-04-25 14:08:53 +0800

    fix gensp's unit tests

--- x/storage/keeper/msg_server_test.go
+++ x/storage/keeper/msg_server_test.go
@@ -1,7 +1,1 @@
 package keeper_test
-
-// nolint: unused
-//func setupMsgServer(t testing.TB) (types.MsgServer, context.Context) {
-//	k, _, ctx := keepertest.StorageKeeper(t)
-//	return keeper.NewMsgServerImpl(*k), sdk.WrapSDKContext(ctx)
-//}
