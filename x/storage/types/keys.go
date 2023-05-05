1371b1e8da1156f6bc61d4f370a0eb36d93f472d
Chris Li <271678682li@gmail.com>
2023-05-05 16:03:53 +0800

    delete unused code

--- x/storage/types/keys.go
+++ x/storage/types/keys.go
@@ -116,10 +116,3 @@ func GetParamsKeyWithTimestamp(timestamp int64) []byte {
 	binary.BigEndian.PutUint64(bz, uint64(timestamp))
 	return append(ParamsKey, bz...)
 }
-
-func ParseParamsKeyWithTimestamp(key []byte) (paramsKey []byte, createTimeSec int64) {
-	length := len(key)
-	paramsKey = key[:length-8]
-	createTimeSec = int64(binary.BigEndian.Uint64(key[length-8 : length]))
-	return
-}
