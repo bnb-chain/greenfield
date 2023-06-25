19487337fecf6468a4b5c6c8c96475295828f88c
Alexxxxxx <118710506+alexgao001@users.noreply.github.com>
2023-06-25 15:59:19 +0800

    add missing field to event (#295)

--- x/storage/keeper/keeper.go
+++ x/storage/keeper/keeper.go
@@ -1102,6 +1102,7 @@ func (k Keeper) CreateGroup(
 		GroupId:    groupInfo.Id,
 		SourceType: groupInfo.SourceType,
 		Members:    opts.Members,
+		Extra:      opts.Extra,
 	}); err != nil {
 		return sdkmath.ZeroUint(), err
 	}
