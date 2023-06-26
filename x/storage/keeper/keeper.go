77a8b884ff3099a7fc4712cef3bcd6f5b116d63d
Chris Li <jingjunLi@users.noreply.github.com>
2023-06-26 10:03:50 +0800

    fix: ACTION_UPDATE_OBJECT_INFO not allowed to be used on object's bug (#285)
    (cherry picked from commit 0899864500138ca5e7d6da5203c1b8d6a535d4b0)

--- x/storage/keeper/keeper.go
+++ x/storage/keeper/keeper.go
@@ -1073,6 +1073,7 @@ func (k Keeper) UpdateObjectInfo(ctx sdk.Context, operator sdk.AccAddress, bucke
 		BucketName: bucketName,
 		ObjectName: objectName,
 		Visibility: visibility,
+		ObjectId:   objectInfo.Id,
 	}); err != nil {
 		return err
 	}
