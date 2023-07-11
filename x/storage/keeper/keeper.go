bc35fe70b7c20273d2c9585fdc489956abd0f134
Fynn <zcheng1004@gmail.com>
2023-07-11 10:13:06 +0800

    fix: add src storage provider to CompleteSwapOut message (#333)

--- x/storage/keeper/keeper.go
+++ x/storage/keeper/keeper.go
@@ -1868,6 +1868,7 @@ func (k Keeper) CompleteMigrateBucket(ctx sdk.Context, operator sdk.AccAddress, 
 		return types.ErrMigrationBucketFailed.Wrapf("err: %s", err)
 	}
 
+	bucketInfo.BucketStatus = types.BUCKET_STATUS_CREATED
 	k.SetBucketInfo(ctx, bucketInfo)
 	k.DeleteMigrationBucketInfo(ctx, bucketInfo.Id)
 
