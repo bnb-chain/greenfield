7f922014e4f3470b2f3a895231c5d8c84bcfe875
Alexgao001 <alex.g@nodereal.io>
2023-07-18 11:41:22 +0800

    lint

--- sdk/client/gnfd_tm_test.go
+++ sdk/client/gnfd_tm_test.go
@@ -2,11 +2,15 @@ package client
 
 import (
 	"context"
+	"testing"
+	"time"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
+
 	"github.com/bnb-chain/greenfield/sdk/client/test"
 	"github.com/bnb-chain/greenfield/sdk/keys"
 	"github.com/stretchr/testify/assert"
-	"testing"
-	"time"
 )
 
 func TestTmClient(t *testing.T) {
@@ -14,17 +18,17 @@ func TestTmClient(t *testing.T) {
 	assert.NoError(t, err)
 	gnfdCli, err := NewGreenfieldClient(test.TEST_RPC_ADDR, test.TEST_CHAIN_ID, WithKeyManager(km), WithWebSocketClient())
 	assert.NoError(t, err)
-	//to, err := sdk.AccAddressFromHexUnsafe(test.TEST_ADDR)
-	//assert.NoError(t, err)
-	//transfer := banktypes.NewMsgSend(km.GetAddr(), to, sdk.NewCoins(sdk.NewInt64Coin(test.TEST_TOKEN_NAME, 12)))
-	//response, err := gnfdCli.BroadcastTx(context.Background(), []sdk.Msg{transfer}, nil)
-	//assert.NoError(t, err)
-	//time.Sleep(2 * time.Second)
-	//// get tx
-	//res, err := gnfdCli.Tx(context.Background(), response.TxResponse.TxHash)
-	//assert.NoError(t, err)
-	//t.Log(res.Hash)
-	//t.Log(res.TxResult.String())
+	to, err := sdk.AccAddressFromHexUnsafe(test.TEST_ADDR)
+	assert.NoError(t, err)
+	transfer := banktypes.NewMsgSend(km.GetAddr(), to, sdk.NewCoins(sdk.NewInt64Coin(test.TEST_TOKEN_NAME, 12)))
+	response, err := gnfdCli.BroadcastTx(context.Background(), []sdk.Msg{transfer}, nil)
+	assert.NoError(t, err)
+	time.Sleep(2 * time.Second)
+	// get tx
+	res, err := gnfdCli.Tx(context.Background(), response.TxResponse.TxHash)
+	assert.NoError(t, err)
+	t.Log(res.Hash)
+	t.Log(res.TxResult.String())
 
 	// get the latest block
 	block, err := gnfdCli.GetBlock(context.Background(), nil)
@@ -47,11 +51,4 @@ func TestTmClient(t *testing.T) {
 	assert.NoError(t, err)
 	t.Log(validators.Validators)
 
-	for i := 1; i < 10000; i++ {
-		h := int64(i)
-		ctx, _ := context.WithTimeout(context.Background(), 15*time.Second)
-		_, err := gnfdCli.GetBlockResults(ctx, &h)
-		assert.NoError(t, err) // why context exceeding is not treated as err
-		t.Log(i)
-	}
 }
