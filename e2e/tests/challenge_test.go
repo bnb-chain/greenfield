59b94b23a65d1985a163a4c09d36f9830298f434
dylanhuang <j75689@gmail.com>
2023-07-19 18:42:04 +0800

    fix: incorrect authority for keepers (#354)
    * fix: incorrect authority for keepers
    
    * fix: lint error
    
    * docs: add virtual group swagger
    
    * chore: fix e2e test
    
    * fix: update e2e test to check params updated
    
    * chore: fix e2e tests

--- e2e/tests/challenge_test.go
+++ e2e/tests/challenge_test.go
@@ -6,6 +6,7 @@ import (
 	"encoding/hex"
 	"fmt"
 	"math"
+	"reflect"
 	"strconv"
 	"strings"
 	"testing"
@@ -15,10 +16,15 @@ import (
 	"github.com/bits-and-blooms/bitset"
 	ctypes "github.com/cometbft/cometbft/rpc/core/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/tx"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
+	v1 "github.com/cosmos/cosmos-sdk/x/gov/types/v1"
 	"github.com/prysmaticlabs/prysm/crypto/bls"
 	"github.com/stretchr/testify/suite"
 
 	"github.com/bnb-chain/greenfield/e2e/core"
+	"github.com/bnb-chain/greenfield/sdk/types"
 	storagetestutil "github.com/bnb-chain/greenfield/testutil/storage"
 	challengetypes "github.com/bnb-chain/greenfield/x/challenge/types"
 	storagetypes "github.com/bnb-chain/greenfield/x/storage/types"
@@ -434,5 +440,98 @@ func filterChallengeEventFromTx(txRes *sdk.TxResponse) challengetypes.EventStart
 		SpOperatorAddress: spOpAddress,
 		RedundancyIndex:   int32(redundancyIndex),
 		ExpiredHeight:     uint64(expiredHeight),
+	}
+}
+
+func (s *ChallengeTestSuite) TestUpdateChallengerParams() {
+	// 1. create proposal
+	govAddr := authtypes.NewModuleAddress(govtypes.ModuleName).String()
+	queryParamsResp, err := s.Client.ChallengeQueryClient.Params(context.Background(), &challengetypes.QueryParamsRequest{})
+	s.Require().NoError(err)
+
+	updatedParams := queryParamsResp.Params
+	updatedParams.HeartbeatInterval = 1800
+	msgUpdateParams := &challengetypes.MsgUpdateParams{
+		Authority: govAddr,
+		Params:    updatedParams,
+	}
+
+	proposal, err := v1.NewMsgSubmitProposal([]sdk.Msg{msgUpdateParams}, sdk.NewCoins(sdk.NewCoin("BNB", sdk.NewInt(1000000000000000000))),
+		s.Validator.GetAddr().String(), "", "update Challenger params", "Test update Challenger params")
+	s.Require().NoError(err)
+	txBroadCastResp, err := s.SendTxBlockWithoutCheck(proposal, s.Validator)
+	s.Require().NoError(err)
+	s.T().Log("create proposal tx hash: ", txBroadCastResp.TxResponse.TxHash)
+
+	// get proposal id
+	proposalID := 0
+	txResp, err := s.WaitForTx(txBroadCastResp.TxResponse.TxHash)
+	s.Require().NoError(err)
+	if txResp.Code == 0 && txResp.Height > 0 {
+		for _, event := range txResp.Events {
+			if event.Type == "submit_proposal" {
+				proposalID, err = strconv.Atoi(event.GetAttributes()[0].Value)
+				s.Require().NoError(err)
+			}
+		}
+	}
+
+	// 2. vote
+	if proposalID == 0 {
+		s.T().Errorf("proposalID is 0")
+		return
+	}
+	s.T().Log("proposalID: ", proposalID)
+	mode := tx.BroadcastMode_BROADCAST_MODE_SYNC
+	txOpt := &types.TxOption{
+		Mode:      &mode,
+		Memo:      "",
+		FeeAmount: sdk.NewCoins(sdk.NewCoin("BNB", sdk.NewInt(1000000000000000000))),
+	}
+	voteBroadCastResp, err := s.SendTxBlockWithoutCheckWithTxOpt(v1.NewMsgVote(s.Validator.GetAddr(), uint64(proposalID), v1.OptionYes, ""),
+		s.Validator, txOpt)
+	s.Require().NoError(err)
+	voteResp, err := s.WaitForTx(voteBroadCastResp.TxResponse.TxHash)
+	s.Require().NoError(err)
+	s.T().Log("vote tx hash: ", voteResp.TxHash)
+	if voteResp.Code > 0 {
+		s.T().Errorf("voteTxResp.Code > 0")
+		return
+	}
+
+	// 3. query proposal until it is end voting period
+CheckProposalStatus:
+	for {
+		queryProposalResp, err := s.Client.Proposal(context.Background(), &v1.QueryProposalRequest{ProposalId: uint64(proposalID)})
+		s.Require().NoError(err)
+		if queryProposalResp.Proposal.Status != v1.StatusVotingPeriod {
+			switch queryProposalResp.Proposal.Status {
+			case v1.StatusDepositPeriod:
+				s.T().Errorf("proposal deposit period")
+				return
+			case v1.StatusRejected:
+				s.T().Errorf("proposal rejected")
+				return
+			case v1.StatusPassed:
+				s.T().Logf("proposal passed")
+				break CheckProposalStatus
+			case v1.StatusFailed:
+				s.T().Errorf("proposal failed, reason %s", queryProposalResp.Proposal.FailedReason)
+				return
+			}
+		}
+		time.Sleep(1 * time.Second)
+	}
+
+	// 4. check params updated
+	err = s.WaitForNextBlock()
+	s.Require().NoError(err)
+
+	updatedQueryParamsResp, err := s.Client.ChallengeQueryClient.Params(context.Background(), &challengetypes.QueryParamsRequest{})
+	s.Require().NoError(err)
+	if reflect.DeepEqual(updatedQueryParamsResp.Params, updatedParams) {
+		s.T().Logf("update params success")
+	} else {
+		s.T().Errorf("update params failed")
 	}
 }
