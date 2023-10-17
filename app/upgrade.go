78ba1c525b8685e8df0fda3cfc668183bf1c879c
yutianwu <wzxingbupt@gmail.com>
2023-10-17 10:40:22 +0800

    fix imports

--- app/upgrade.go
+++ app/upgrade.go
@@ -1,15 +1,15 @@
 package app
 
 import (
-	bridgemoduletypes "github.com/bnb-chain/greenfield/x/bridge/types"
-	storagemoduletypes "github.com/bnb-chain/greenfield/x/storage/types"
 	serverconfig "github.com/cosmos/cosmos-sdk/server/config"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
 	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
 
+	bridgemoduletypes "github.com/bnb-chain/greenfield/x/bridge/types"
 	paymentmodule "github.com/bnb-chain/greenfield/x/payment"
 	paymenttypes "github.com/bnb-chain/greenfield/x/payment/types"
+	storagemoduletypes "github.com/bnb-chain/greenfield/x/storage/types"
 )
 
 func (app *App) RegisterUpgradeHandlers(chainID string, serverCfg *serverconfig.Config) error {
