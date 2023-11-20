db6f3f0a0fed42daa19f5b5f8f9d7ad631928fe9
Roshan <48975233+Pythonberg1997@users.noreply.github.com>
2023-11-20 16:35:35 +0800

    feat: introduce the Eddystone upgrade (#525)

--- app/upgrade.go
+++ app/upgrade.go
@@ -22,6 +22,7 @@ func (app *App) RegisterUpgradeHandlers(chainID string, serverCfg *serverconfig.
 	// Register the upgrade handlers here
 	app.registerNagquUpgradeHandler()
 	app.registerPampasUpgradeHandler()
+	app.registerEddystoneUpgradeHandler()
 	// app.register...()
 	// ...
 	return nil
@@ -101,6 +102,24 @@ func (app *App) registerPampasUpgradeHandler() {
 
 			// enable chain id for opbnb
 			app.CrossChainKeeper.SetDestOpChainID(sdk.ChainID(app.appConfig.CrossChain.DestOpChainId))
+			return nil
+		})
+}
+
+func (app *App) registerEddystoneUpgradeHandler() {
+	// Register the upgrade handler
+	app.UpgradeKeeper.SetUpgradeHandler(upgradetypes.Eddystone,
+		func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
+			app.Logger().Info("upgrade to ", plan.Name)
+
+			return app.mm.RunMigrations(ctx, app.configurator, fromVM)
+		})
+
+	// Register the upgrade initializer
+	app.UpgradeKeeper.SetUpgradeInitializer(upgradetypes.Eddystone,
+		func() error {
+			app.Logger().Info("Init Eddystone upgrade")
+
 			return nil
 		})
 }
